a171f16ec59ff3778fa290d1399c5ad7a34e9d1c
ThomasA64 <thomas.arthur64@gmail.com>
2021-01-13 16:32:53 +0000

    added a bit of css

--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,23 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const menuRoute  = express.Router();
+const express = require("express");
+const menuRoute = express.Router();
 
 module.exports = (db) => {
   menuRoute.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`) //change this to dishes
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   menuRoute.post("/", (req, res) => {
     // access the user input to the cart.  is this an object?  or a Database access query?
-
-  })
+  });
   return menuRoute;
-
 };
